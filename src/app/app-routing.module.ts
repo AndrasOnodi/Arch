3008d92538bb7f4be75844acbb5ebd66ffd269c9
Ónodi András <onodiandraspeter@ktch.hu>
2025-02-21 15:22:10 +0100

    Ketto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { BookingComponent } from './booking/booking.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'booking', component: BookingComponent},
+  {path: 'reservation', component: ReservationComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sigh-up', component: SignUpComponent},
+  {path: 'sign-out', component: SignOutComponent},
+  {path: '', redirectTo: 'sign-up', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { ReservationComponentComponent } from './reservation-component/reservation-component.component';
 import { HomeComponent } from './home/home.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { BookingComponent } from './booking/booking.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environment/environment';
+import { provideHttpClient } from '@angular/common/http';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
-    ReservationComponentComponent,
     HomeComponent,
     ReservationComponent,
     BookingComponent,
     UserComponent,
-    AdminComponent
+    AdminComponent,
+    NavbarComponent,
+    SignUpComponent,
+    SignInComponent,
+    SignOutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
